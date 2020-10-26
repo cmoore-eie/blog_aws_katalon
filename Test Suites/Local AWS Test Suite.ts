17b92769a5b667fb2ceb147552f25911fafe0cc7
Chris Moore <cmoore@eie.pl>
2020-10-26 10:42:26 +0100

    Updates for Remote Tests

--- Test Suites/Local AWS Test Suite.ts
+++ Test Suites/Local AWS Test Suite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>AWS Test Suite</name>
+   <name>Local AWS Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -15,18 +15,18 @@
       <guid>946c292a-9444-41d6-aa59-b59691e98a45</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Branch Test Case</testCaseId>
+      <testCaseId>Test Cases/Local/Branch Test Case</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9770d9e6-e4cb-4ed3-899a-3720a3fcf329</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Gender Test Case</testCaseId>
+      <testCaseId>Test Cases/Local/Gender Test Case</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>efdc45a0-3f18-47f6-b7eb-3f2f939c925a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Dealer Test Case</testCaseId>
+      <testCaseId>Test Cases/Local/Dealer Test Case</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
---
+++ Test Suites/Remote AWS Test Suite.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Remote AWS Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>68e0395e-43fe-4a93-9d4d-8071d3d336de</testSuiteGuid>
+   <testCaseLink>
+      <guid>9770d9e6-e4cb-4ed3-899a-3720a3fcf329</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Remote/Gender Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>946c292a-9444-41d6-aa59-b59691e98a45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Remote/Branch Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>efdc45a0-3f18-47f6-b7eb-3f2f939c925a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Remote/Dealer Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
